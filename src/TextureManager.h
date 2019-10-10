7679b29fb85b318ee911af8ad69936f468fd6fc6
Edward Deam <edwardcrdeam@gmail.com>
2019-10-10 16:15:05 +1100

    Asset Management and Texture loading

---
+++ src/TextureManager.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "./Game.h"
+
+class TextureManager
+{
+    public:
+        static SDL_Texture* LoadTexture(const char* filename);
+        static void Draw(SDL_Texture* texture, SDL_Rect sourceRectangle, SDL_Rect destinationRectangle, SDL_RendererFlip flip);
+};
